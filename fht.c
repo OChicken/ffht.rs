22c8678ccc5c8382587ab984cac9259718117a3c
OChicken <shouran.ma@gmail.com>
2025-11-12 09:38:43 +0100

    this fht.{c,h} can make Rust test all passed

--- fht.c
+++ fht.c
@@ -1,72 +1,12 @@
 #include "fht.h"
-#include <string.h>
-
-// Fast copy implementation with SIMD optimizations
-// Include architecture-specific headers
-#if (defined(__x86_64__) || defined(__i386__))
-#  include <x86intrin.h>
-#elif (defined(__aarch64__) || defined(__ARM_NEON))
-#  include <arm_neon.h>
-#endif
-
-#define FAST_COPY_MEMCPY_THRESHOLD ((size_t)1ull << 20)
-
-void *fast_copy(void *out, void *in, size_t n) {
-    // For large copies, use memcpy
-    if (n >= FAST_COPY_MEMCPY_THRESHOLD) {
-        return memcpy(out, in, n);
-    }
-
-#if defined(__AVX2__)
-    // AVX2: 256-bit vectors (32 bytes)
-    if (n >= 32) {
-        size_t count = n >> 5;
-        __m256 *ov = (__m256 *)out;
-        __m256 *iv = (__m256 *)in;
-        for (; count--; ) {
-            _mm256_storeu_ps((float *)(ov++), _mm256_loadu_ps((float *)(iv++)));
-        }
-        return out;
-    }
-#endif
-
-#if defined(__SSE2__) || defined(__x86_64__)
-    // SSE2: 128-bit vectors (16 bytes) - enabled by default on x86_64
-    if (n >= 16) {
-        size_t count = n >> 4;
-        __m128 *ov = (__m128 *)out;
-        __m128 *iv = (__m128 *)in;
-        for (; count--; ) {
-            _mm_storeu_ps((float *)(ov++), _mm_loadu_ps((float *)(iv++)));
-        }
-        return out;
-    }
-#endif
-
-#if defined(__aarch64__) || defined(__ARM_NEON)
-    // ARM NEON: 128-bit vectors (16 bytes)
-    if (n >= 16) {
-        size_t count = n >> 4;
-        float *ov = (float *)out;
-        const float *iv = (const float *)in;
-        for (; count--; ov += 4, iv += 4) {
-            vst1q_f32(ov, vld1q_f32(iv));
-        }
-        return out;
-    }
-#endif
-
-    // Fallback for small sizes or unsupported architectures
-    return memcpy(out, in, n);
-}
 
 // Include SIMD implementation
 #ifdef __AVX__
-#include "fht_avx.c"
+#include "FFHT/fht_avx.c"
 #elif defined(__aarch64__) || defined(__ARM_NEON)
 #include "fht_neon.c"
 #else
-#include "fht_sse.c"
+#include "FFHT/fht_sse.c"
 #endif
 
 // Define out-of-place functions here (after fast_copy is defined)
--- fht.h
+++ fht.h
@@ -2,9 +2,94 @@
 #define _FHT_H_
 #include <string.h>
 #include <stdlib.h>
+#if (defined(__x86_64__) || defined(__i386__))
+#  include <x86intrin.h>
+#elif (defined(__aarch64__) || defined(__ARM_NEON))
+#  include <arm_neon.h>
+#endif
+
+#ifndef FAST_COPY_MEMCPY_THRESHOLD
+#  define FAST_COPY_MEMCPY_THRESHOLD ((size_t)1ull << 20)
+#endif
+
+#ifdef FHT_HEADER_ONLY
+#  define _STORAGE_ static inline
+#else
+#  define _STORAGE_
+#endif
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+// These functions all assume that the size of memory being copied is a power of 2.
+
+#if _FEATURE_AVX512F
+// If n is less than 64, defaults to memcpy. Otherwise, being a power of 2, we can just use unaligned stores and loads.
+_STORAGE_ void *fast_copy(void *out, void *in, size_t n) {
+    if(n >= FAST_COPY_MEMCPY_THRESHOLD) {
+        return memcpy(out, in, n);
+    }
+    n >>= 6;
+    for(__m512 *ov = (__m512 *)out, *iv = (__m512 *)in; n--;) {
+        _mm512_storeu_ps((float *)(ov++), _mm512_loadu_ps((float *)(iv++)));
+    }
+    return out;
+}
+#elif __AVX2__
+// If n is less than 32, defaults to memcpy. Otherwise, being a power of 2, we can just use unaligned stores and loads.
+_STORAGE_ void *fast_copy(void *out, void *in, size_t n) {
+    if(n >= FAST_COPY_MEMCPY_THRESHOLD) {
+        return memcpy(out, in, n);
+    }
+    if (n >= 32) {
+        size_t count = n >> 5;
+        __m256 *ov = (__m256 *)out;
+        __m256 *iv = (__m256 *)in;
+        for(; count--;) {
+            _mm256_storeu_ps((float *)(ov++), _mm256_loadu_ps((float *)(iv++)));
+        }
+        return out;
+    }
+    return memcpy(out, in, n);
+}
+#elif __SSE2__
+// If n is less than 16, defaults to memcpy. Otherwise, being a power of 2, we can just use unaligned stores and loads.
+_STORAGE_ void *fast_copy(void *out, void *in, size_t n) {
+    if(n >= FAST_COPY_MEMCPY_THRESHOLD) {
+        return memcpy(out, in, n);
+    }
+    n >>= 4;
+    for(__m128 *ov = (__m128 *)out, *iv = (__m128 *)in; n--;) {
+        _mm_storeu_ps((float *)(ov++), _mm_loadu_ps((float *)(iv++)));
+    }
+    return out;
+}
+#elif defined(__aarch64__) || defined(__ARM_NEON)
+// ARM NEON: 128-bit vectors (16 bytes)
+_STORAGE_ void *fast_copy(void *out, void *in, size_t n) {
+    if(n >= FAST_COPY_MEMCPY_THRESHOLD) {
+        return memcpy(out, in, n);
+    }
+    if (n >= 16) {
+        size_t count = n >> 4;
+        float *ov = (float *)out;
+        const float *iv = (const float *)in;
+        for(; count--; ov += 4, iv += 4) {
+            vst1q_f32(ov, vld1q_f32(iv));
+        }
+        return out;
+    }
+    return memcpy(out, in, n);
+}
+#else
+_STORAGE_ void *fast_copy(void *out, void *in, size_t n) {
+    return memcpy(out, in, n);
+}
+#endif
+
+#ifdef FHT_HEADER_ONLY
+#  undef _STORAGE_
 #endif
 
 int fht_float(float *buf, int log_n);
